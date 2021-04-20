41bb729c74c3a95745707071e8b5297d10d137a5
yangminxin <903435532@qq.com>
2021-04-20 17:13:54 +0800

    feat: gripper_server done

--- innfos_control/gripper_control/src/gripper_servers.cpp
+++ innfos_control/gripper_control/src/gripper_servers.cpp
@@ -2,7 +2,7 @@
 #include "actionlib/server/action_server.h"
 //action服务端的目标控制句柄定义，与接收的目标相关联后，可以用来实现action的信息反馈等操作
 #include "actionlib/server/server_goal_handle.h"
-#include "control_msgs/GripperCommand.h"
+#include "control_msgs/GripperCommandAction.h"
 #include "ros/ros.h"
 #include "SCServo.h"
 
@@ -18,14 +18,14 @@ protected:
 
     ros::NodeHandle nh_;
     //定义action服务端
-    actionlib::ActionServer<control_msgs::GripperCommand>  as_;
+    actionlib::ActionServer<control_msgs::GripperCommandAction>  as_;
     //定义action服务端目标控制句柄
-    actionlib::ServerGoalHandle<control_msgs::GripperCommand> goal_handle_;
+    actionlib::ServerGoalHandle<control_msgs::GripperCommandAction> goal_handle_;
     //用来反馈action目标的执行情况，客户端由此可以得知服务端是否执行成功了
-    control_msgs::GripperCommand result_;
+    control_msgs::GripperCommandAction result_;
 
     std::string action_name_;
-
+    std::string port_;
 public:
     SMSBL sm;
     GripperService(std::string name) :
@@ -33,23 +33,25 @@ public:
             action_name_(name)
     {
         as_.start();
+        nh_.param<std::string>("servo_port", port_, "/dev/ttyUSB0");
+        ROS_INFO("[gripper_control] serial: %s",port_.c_str());
         if(!sm.begin(115200, port_.c_str())){
             ROS_ERROR("[gripper_service] Failed to init smsbl motor!");
         }
     }
 
-    void goalCB( actionlib::ServerGoalHandle<control_msgs::GripperCommand> gh)
+    void goalCB( actionlib::ServerGoalHandle<control_msgs::GripperCommandAction> gh)
     {
         //疑点1：什么要将形参复制一份？
         //因为我们要修改这个参数，但又不#include "../include/MotorDriver.h"能直接改形参所指向的那个目标对象，那样做会破坏原始数据，是编程所不推荐的，这里复制一个副本，目的就是要修改这个数据而不破坏原始数据。
-        actionlib::ActionServer<control_msgs::GripperCommand>::Goal goal = *gh.getGoal();    //make a copy that we can modify
+        actionlib::ActionServer<control_msgs::GripperCommandAction>::Goal goal = *gh.getGoal();    //make a copy that we can modify
 
         //将之前定义的句柄与传入的action目标绑定在一起，
         goal_handle_ = gh;
 
         //excute
         int pos;
-        if(goal.position == 0){
+        if(goal.command.position == 0){
             pos = 22;
         }
         else{
@@ -59,9 +61,9 @@ public:
         usleep(800*1000);
         ROS_INFO("[gripper_control] write pos: %d",pos);
         //告诉客户端成功了
-        goal_handle_.setAccepted();
-        result_.error_code = result_.SUCCESSFUL;
-        goal_handle_.setSucceeded(result_);
+        // goal_handle_.setAccepted();
+        // result_.command.reached_goal = true;
+        // goal_handle_.setSucceeded(result_);
     }
 };
 
